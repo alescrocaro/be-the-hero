4e84ac59bb405a238644997af3b234241bf2cf9f
alescrocaro <alescrocaro@gmail.com>
2022-03-31 19:41:59 -0300

    iniciando configs de api

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Logon from '../pages/logon';
-import Register from '../pages/register';
+import Logon from '../pages/Logon';
+import Register from '../pages/Register';
 import Profile from '../pages/Profile';
 import NewIncident from '../pages/NewIncident';
 
